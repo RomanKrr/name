31e0acb2e647e8787b388c0d166765a9d31b30e6
Your Name <you@example.com>
2024-02-15 14:02:43 +0200

    first commit

---
+++ js/hi.js
@@ -0,0 +1,21 @@
+// Знаходимо всі блоки з класом "product"
+/*var products = document.querySelectorAll('.product');
+
+// Проходимо циклом по кожному блоку і додаємо обробник події "mouseover"
+for (var i = 0; i < products.length; i++) {
+  products[i].addEventListener('mouseover', function()) {
+    // Отримуємо назву та ціну товару з атрибутів "data-name" та "data-price"
+    var name = this.getAttribute('data-name');
+    var price = this.getAttribute('data-price');
+ }
+
+    // Додаємо вміст для показу назви та ціни товару
+    this.setAttribute('data-name', '');
+    this.setAttribute('data-price', '');
+
+    // Додаємо клас "active", щоб активувати CSS-стилі для показу інформації
+    this.classList.add('active');
+  });
+
+  // Додаємо*/
+  
